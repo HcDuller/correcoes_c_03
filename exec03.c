5db77d6cd20301e1577bae962254438a6848cfaf
hde-camp <hde-camp@student.42sp.org.br>
2021-04-13 15:25:58 +0000

    Alteracoes em exec03.c

--- exec03.c
+++ exec03.c
@@ -6,7 +6,7 @@
 /*   By: hde-camp <hde-camp@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 22:35:15 by hde-camp          #+#    #+#             */
-/*   Updated: 2021/04/13 01:10:45 by hde-camp         ###   ########.fr       */
+/*   Updated: 2021/04/13 15:25:05 by hde-camp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,44 @@ int	main(void)
 	char *s2;
 	int n;
 
-	s1 = calloc(50, sizeof(char));
-	s2 = calloc(50, sizeof(char));
-	n = 10;
+	s1 = calloc(26, sizeof(char));
+	s2 = calloc(13, sizeof(char));
+	
 
 	printf("================================EX03===================================\n");
-	printf("=======================================================================\n\n");
+	printf("=======================================================================\n");
 	strcpy(s1, "This is Dest:");
-	strcpy(s2, "This is Src.");	
-	printf("		src: %s	dest: %s	\n\n", s2, s1);
+	strcpy(s2, "This is Src.");
+	n = 13;
+	printf("		src: %s	dest: %s	\n", s2, s1);
     strncat(s1, s2, n);
     printf("		strcat(n=%d):		%s\n", n, s1);	
 	strcpy(s1, "This is Dest:");
 	strcpy(s2, "This is Src.");	
 	ft_strncat(s1, s2, n);
-	printf("		ft_strncat(n=%d):	%s\n\n", n, s1);
+	printf("		ft_strncat(n=%d):	%s\n", n, s1);
 	printf("=======================================================================\n");
+	strcpy(s1, "This is Dest:");
+	strcpy(s2, "This is Src.");
+	n = 5;
+	printf("		src: %s	dest: %s	\n", s2, s1);
+    strncat(s1, s2, n);
+    printf("		strcat(n=%d):		%s\n", n, s1);	
+	strcpy(s1, "This is Dest:");
+	strcpy(s2, "This is Src.");	
+	ft_strncat(s1, s2, n);
+	printf("		ft_strncat(n=%d):	%s\n", n, s1);
+	printf("=======================================================================\n");
+	strcpy(s1, "This is Dest:");
+	strcpy(s2, "This is Src.");
+	n = 0;
+	printf("		src: %s	dest: %s	\n", s2, s1);
+    strncat(s1, s2, n);
+    printf("		strcat(n=%d):		%s\n", n, s1);	
+	strcpy(s1, "This is Dest:");
+	strcpy(s2, "This is Src.");	
+	ft_strncat(s1, s2, n);
+	printf("		ft_strncat(n=%d):	%s\n", n, s1);	
 	printf("================================FIM====================================\n");
 
 	free(s1);
